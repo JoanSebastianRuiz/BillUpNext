0c22dcf3d2cd8d3d5c7e5b6850c97abf29139826
erica sisa <vanesasisapinzon1999@gmail.com>
2025-02-26 18:42:43 -0500

    feat: :sparkles: script categoria

---
+++ DataBase/scripts/categorias/funCategoria.sql
@@ -0,0 +1,67 @@
+CREATE OR REPLACE FUNCTION insertarCategoria(
+    _nombreCategoria "Categoria"."nombreCategoria"%TYPE,
+    _estadoCategoria "Categoria"."estadoCategoria"%TYPE) RETURNS BOOLEAN AS
+$BODY$
+DECLARE
+    _idCategoria "Categoria"."idCategoria"%TYPE;
+BEGIN
+    IF EXISTS (SELECT 1 FROM "Categoria" WHERE LOWER("nombreCategoria") = LOWER(_nombreCategoria)) THEN
+        RAISE EXCEPTION 'El nombre de la categoria % ya existe. Inserción cancelada.',_nombreCategoria;
+    END IF;
+
+        INSERT INTO "Categoria" ("nombreCategoria", "estadoCategoria")
+        VALUES (_nombreCategoria,_estadoCategoria);
+
+        IF FOUND THEN
+            RAISE NOTICE 'Se insertó correctamente la categoria';
+			RETURN TRUE;
+		ELSE
+            RAISE NOTICE 'Ocurrió un error';
+			RETURN FALSE;
+        END IF;
+     
+END;
+$BODY$
+LANGUAGE PLPGSQL;
+
+
+CREATE OR REPLACE FUNCTION actualizarCategoria(
+    _idCategoria "Categoria"."idCategoria"%TYPE,
+    _nombreCategoria "Categoria"."nombreCategoria"%TYPE,
+    _estadoCategoria "Categoria"."estadoCategoria"%TYPE) 
+    RETURNS BOOLEAN AS
+$BODY$
+BEGIN
+    UPDATE "Categoria"
+    SET   "nombreCategoria" = _nombreCategoria,
+          "estadoCategoria" = _estadoCategoria
+    WHERE "idCategoria"  = _idCategoria;
+
+    IF FOUND THEN
+        RAISE NOTICE 'Se actualizó correctamente la categoria';
+        RETURN TRUE;
+    ELSE
+        RAISE NOTICE 'Ocurrió un error al actualizar la categoria';
+        RETURN FALSE;
+    END IF;
+END;
+$BODY$
+LANGUAGE PLPGSQL;
+
+
+CREATE OR REPLACE FUNCTION existeCategoriaNombre(
+    _nombreCategoria "Categoria"."nombreCategoria"%TYPE,
+    _idCategoria "Categoria"."idCategoria"%TYPE DEFAULT NULL
+)
+RETURNS BOOLEAN AS
+$BODY$
+BEGIN
+    RETURN EXISTS (
+        SELECT 1
+        FROM "Categoria"
+        WHERE LOWER("nombreCategoria") = LOWER(_nombreCategoria)
+        AND (_idCategoria IS NULL OR "idCategoria" != _idCategoria)
+    );
+END;
+$BODY$
+LANGUAGE PLPGSQL;
