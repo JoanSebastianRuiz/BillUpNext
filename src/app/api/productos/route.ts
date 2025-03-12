5872b95bd948c4ace9ecf2467305105ac14a5740
esisa9 <vanesasisapinzon1999@gmail.com>
2025-03-12 10:56:00 -0500

    feat: :recycle: route producto

--- src/app/api/productos/route.ts
+++ src/app/api/productos/route.ts
@@ -1,7 +1,5 @@
 import { ProductoServiceImpl } from "@/services/Impl/ProductoServiceImpl";
 
-
-
 export const POST = async (request: Request) => {
   const productoService = ProductoServiceImpl.getInstance();
   const respuesta = await productoService.create(await request.json());
