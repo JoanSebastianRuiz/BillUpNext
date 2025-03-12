3f66f34788c85d6094a4a6ec7277541572b83787
esisa9 <vanesasisapinzon1999@gmail.com>
2025-03-12 06:43:07 -0500

    feat: :recycle: route, context, DAO y service de producto

--- src/services/ProductoService.ts
+++ src/services/ProductoService.ts
@@ -3,7 +3,7 @@ import { ProductoResponseDTO } from "@/dto/ProductoResponseDTO";
 import { ProductoRequestDTO } from "@/dto/ProductoRequestDTO";
 
 export interface ProductoService {
-    getAll(): Promise<Array<ProductoResponseDTO>>;
+    getAll(idEmpresa: number): Promise<Array<ProductoResponseDTO>>;
     create(producto: ProductoRequestDTO): Promise<NextResponse>;
     update(producto: ProductoRequestDTO): Promise<NextResponse>;
     getById(idProducto: number): Promise<ProductoResponseDTO | null>;
