7827f2e27cce3c6b18af95f30b480a7798fea74a
esisa9 <vanesasisapinzon1999@gmail.com>
2025-03-12 06:45:58 -0500

    feat: :recycle: route categoria

--- src/app/api/categorias/route.ts
+++ src/app/api/categorias/route.ts
@@ -1,19 +1,4 @@
-import { NextResponse } from "next/server";
 import { CategoriaServiceImpl } from "@/services/Impl/CategoriaServiceImpl";
-
-export const GET = async () => {
-  try {
-    const categoriaService = CategoriaServiceImpl.getInstance();
-    const respuesta = await categoriaService.getAll();
-    return NextResponse.json(respuesta, { status: 200 });
-  } catch (error) {
-    console.error("Error al obtener las categorias:", error);
-    return NextResponse.json(
-      { message: "Error al obtener las categorías" },
-      { status: 500 }
-    );
-  }
-};
 
 export const POST = async (request: Request) => {
   
